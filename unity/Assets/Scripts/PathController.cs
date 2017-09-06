046dff7e804c87f75eb8e8b9d6646fc1d69b418d
Dimitris Doukas <dimitris@baud.tech>
2017-09-06 05:24:50 +0300

    arcore: better placement of elements in the scene using ARCore anchoring

--- unity/Assets/Scripts/PathController.cs
+++ unity/Assets/Scripts/PathController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using GoogleARCore;
 
 /*********************************************************************/
 
@@ -29,6 +30,7 @@ public class PathController : MonoBehaviour
 	public RobotController robotController;
 
     private ARController arController;
+    private float groundPlaneY = -1f;
     private GameObject reticle;
 	private GameObject calibrator;
 	private List<Vector2> pathPoints;
@@ -36,8 +38,8 @@ public class PathController : MonoBehaviour
 	private GameObject currentUndoButton;
 	private Vector2 robotPosition;
 	private float robotHeading;
-	private Plane groundPlane;
 	private float undoTime;
+    private Camera arCamera;
 
 	private enum State
 	{
@@ -63,7 +65,8 @@ public class PathController : MonoBehaviour
 	{
 		// initilizations
 		arController = FindObjectOfType<ARController> ();
-		pathPoints = new List<Vector2> ();
+        arCamera = arController.m_firstPersonCamera;
+        pathPoints = new List<Vector2> ();
 		pathMarkers = new List<GameObject> ();
 		state = State.FindingFloor;
 		undoTime = 0.0f;
@@ -73,7 +76,7 @@ public class PathController : MonoBehaviour
 	{
 		// find the ground plane and place a reticle on it - this will only be called once
 		if (state == State.FindingFloor && arController.FirstGroundPlane != null) {
-			groundPlane = new Plane (Vector3.up, new Vector3 (0, arController.FirstGroundPlane.Position.y, 0));
+            groundPlaneY = arController.FirstGroundPlane.Position.y;
 			state = State.Placing;
 			reticle = Instantiate (placementReticlePrefab);
 		}
@@ -94,29 +97,30 @@ public class PathController : MonoBehaviour
             }
         }
     }
-
+    
 	public void LateUpdate()
 	{
         // update the reticle's position on the ground
         if (state == State.Placing || state == State.Calibrating)
         {
-            Transform cam = arController.m_firstPersonCamera.transform;
-            Ray ray = new Ray(cam.position, cam.forward);
-            float rayDistance;
-            if (groundPlane.Raycast(ray, out rayDistance))
+            TrackableHit hit;
+            TrackableHitFlag raycastFilter = TrackableHitFlag.PlaneWithinBounds | TrackableHitFlag.PlaneWithinPolygon;
+            Ray ray = new Ray(arCamera.transform.position, arCamera.transform.forward);
+            if (Session.Raycast(ray, raycastFilter, out hit))
             {
-                reticle.transform.position = ray.GetPoint(rayDistance);
-                float tangoYaw = cam.rotation.eulerAngles.y;
+                // cast a ray to the middle of the screen and place the reticle there
+                reticle.transform.position = hit.Point;
+                float tangoYaw = arCamera.transform.rotation.eulerAngles.y;
                 reticle.transform.rotation = Quaternion.Euler(0, tangoYaw, 0);
             }
         }
 	}
-
+    
     // calibrate the robot's position/orientation based on the calibrator object's transform
 	private void CalibrateRobot(Vector3 position)
 	{
 		robotPosition = new Vector2 (position.x, position.z);
-		robotHeading = arController.m_firstPersonCamera.transform.rotation.eulerAngles.y;
+		robotHeading = arCamera.transform.rotation.eulerAngles.y;
 		Quaternion robotQuaternion = Quaternion.Euler (0, robotHeading, 0);
         if (calibrator == null) {
             calibrator = Instantiate(calibrationReticlePrefab, position, robotQuaternion);
@@ -128,25 +132,51 @@ public class PathController : MonoBehaviour
 	// add a point to the road path
 	public void AddPoint(Vector3 pointPosition)
 	{
-		pathPoints.Add (new Vector2 (pointPosition.x, pointPosition.z));
-		GameObject marker;
-		if (pathMarkers.Count == 0) {
-			marker = firstPointPrefab;
-		} else {
-			marker = pointPrefab;
-		}
-		pathMarkers.Add (Instantiate (marker, pointPosition, Quaternion.identity));
-		if (pathMarkers.Count == 1) {
-			Animation anim = pathMarkers [0].GetComponent<Animation> ();
-			anim.Play ("animIn");
-			anim.PlayQueued ("idle", QueueMode.CompleteOthers);
-			pointPosition.y += 0.3f;
-		}
-		if (currentUndoButton) {
-			Destroy (currentUndoButton);
-		}
-		currentUndoButton = Instantiate (undoButtonPrefab, pointPosition, Quaternion.identity);
-		UpdateLineRenderer ();
+		// find the point on the detected planes where the point should be placed
+        TrackableHit hit;
+        TrackableHitFlag raycastFilter = TrackableHitFlag.PlaneWithinBounds | TrackableHitFlag.PlaneWithinPolygon;
+        Ray ray = new Ray(arCamera.transform.position, pointPosition - arCamera.transform.position);
+        if (Session.Raycast(ray, raycastFilter, out hit))
+        {
+            // add point to collection
+            pathPoints.Add(new Vector2(pointPosition.x, pointPosition.z));
+            // select the right prefab for the marker
+            GameObject marker;
+            if (pathMarkers.Count == 0)
+            {
+                marker = firstPointPrefab;
+            }
+            else
+            {
+                marker = pointPrefab;
+            }
+
+            // create an anchor to allow ARCore to track the hitpoint as understanding of the physical world evolves
+            var anchor = Session.CreateAnchor(hit.Point, Quaternion.identity);
+
+            // Intanstiate the object as a child of the anchor; it's transform will now benefit from the anchor's tracking
+            GameObject newMarker = Instantiate(marker, hit.Point, Quaternion.identity, anchor.transform);
+            pathMarkers.Add(newMarker);
+
+            // play animations on the markers
+            if (pathMarkers.Count == 1)
+            {
+                Animation anim = pathMarkers[0].GetComponent<Animation>();
+                if (anim)
+                {
+                    anim.Play("animIn");
+                    anim.PlayQueued("idle", QueueMode.CompleteOthers);
+                }
+            }
+            // place the undo button
+            if (currentUndoButton)
+            {
+                Destroy(currentUndoButton);
+            }
+            currentUndoButton = Instantiate(undoButtonPrefab, hit.Point + Vector3.up * 0.03f, Quaternion.identity);
+            // update the path line
+            UpdateLineRenderer();
+        }
 	}
 
 	// update the line renderer visualizing the path
@@ -191,7 +221,6 @@ public class PathController : MonoBehaviour
 				}
 			}
 		}
-
 		pathMarkers.RemoveAt (lastIndex);
 		UpdateLineRenderer ();
 	}
@@ -260,9 +289,9 @@ public class PathController : MonoBehaviour
 	}
 
     // returns the height of the ground plane in the Unity world
-	public float FloorHeight()
+    // TODO: remove this altogether and only use anchors to place things in the world in the methods where this is used
+    public float FloorHeight()
 	{
-		if(arController.FirstGroundPlane != null) return arController.FirstGroundPlane.Position.y;
-		else return -1.0f;
+		return groundPlaneY;
 	}
 }
